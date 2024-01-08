f715740643c5331a02a41b15cd5f1a1ea6584893
Matthew Milam <96691690+08milam@users.noreply.github.com>
2024-01-08 09:13:09 -0600

    local starage

--- script.js
+++ script.js
@@ -1,100 +1,92 @@
-
-
 /*Made By: Charles M Milam Jr |  | ToDo.css*/
 
 //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 // TOP MENU BAR
-let menu = function(){
+function menu() {
   let targetElement = document.querySelector(".navbar");
-let halfwayPoint = window.innerHeight / 35;
-window.onscroll = function() {
-  let scrollPosition = window.scrollY;
-  if (scrollPosition >= halfwayPoint) {
-    targetElement.style.backgroundColor = "#2E5266FF"; 
-  } else {
-    targetElement.style.backgroundColor = "transparent";
-  }
-};
+  let halfwayPoint = window.innerHeight / 35;
+  window.onscroll = function () {
+    let scrollPosition = window.scrollY;
+    if (scrollPosition >= halfwayPoint) {
+      targetElement.style.backgroundColor = "#2E5266FF";
+    } else {
+      targetElement.style.backgroundColor = "transparent";
+    }
+  };
 }
 
 //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 // CREATE ITEM
-let create = function(){
 
-  tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-  
-  inputElement = document.querySelector('#input');
-  inputValue = inputElement.value;
-  const displayItem = document.querySelector('#toDoList');
-  newItem = document.createElement("li")
-      newItem.setAttribute("class", "item")
-      newItem.textContent = inputValue
-      displayItem.prepend(newItem)
+function create(inputValue) {
+  const displayItem = document.querySelector("#toDoList");
+  const newItem = document.createElement("li");
+  newItem.setAttribute("class", "item");
+  newItem.textContent = inputValue;
+  displayItem.prepend(newItem);
 
-      tasks.forEach(a)
-      function a(element){
-      }
+  // add x delete btn
+  const remove = document.createElement("button");
+  remove.setAttribute("class", "remove");
+  remove.textContent = "X";
+  newItem.appendChild(remove);
+  remove.addEventListener("click", function (e) {
+    e.target.parentElement.remove();
+    // todos = todos.filter(t => )
+    removeFromStorage(inputValue);
+  });
 }
 
+function removeFromStorage(inputValue) {
+  let tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+
+  let index = tasks.findIndex((element) => element.content === inputValue);
+  tasks.splice(index, 1);
+  console.log(tasks);
+  console.log(index);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 // STORING ITEMS IN MEMORY
-let storage = function(){
-const todo = {
-  content : inputValue 
-}
-  tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+let storage = function (inputValue) {
+  testStorage();
+  const todo = {
+    content: inputValue,
+  };
+  let tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+  console.log(tasks);
   tasks.push(todo);
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-}
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
 
 //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 // TEST LOCAL STORAGE
-let testStorage = function(){
-  if (typeof(Storage) !== "undefined") {
+function testStorage() {
+  if (typeof Storage !== "undefined") {
   } else {
-      console.error("LocalStorage is not supported in this browser.");
+    console.error("LocalStorage is not supported in this browser.");
   }
 }
 
-//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-// MARK ITEM AS COMPLETE
-let completed = function(){
-    newItem.addEventListener('click', function(e) {
-    if (e.target.tagName === 'LI') {
-      e.target.classList.toggle('completed');
-    }
+function loadItem() {
+  let tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+  tasks.forEach((element) => {
+    create(element.content);
   });
 }
 
 //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-// REMOVE ITEM
-let drop = function(){
-  const remove = document.createElement('button')
-    remove.setAttribute("class", "remove")
-    remove.textContent = "X"
-    newItem.appendChild(remove)
-    remove.addEventListener('click', function(e){
-              e.target.parentElement.remove(); 
-              // todos = todos.filter(t => ) 
-              localStorage.setItem('tasks', JSON.stringify(tasks));})
-}
-
-//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 // COMBINE ALL FUNCTION
-const form = document.querySelector('#listItems');
-form.addEventListener('submit', function(e) {
+const form = document.querySelector("#listItems");
+form.addEventListener("submit", function (e) {
   e.preventDefault();
+  let inputElement = document.querySelector("#input");
+  let inputValue = inputElement.value;
+  menu();
 
-  menu() 
+  create(inputValue);
 
-  create() 
-
-  drop() 
-
-  completed()
-
-  testStorage() 
-
-  // storage() <---- is turned off
+  storage(inputValue);
 });
+loadItem();
